394e59be8596e39c448c96190f6832c437f6d199
niyaz-n <niyaznouzz@gmail.com>
2024-03-28 00:27:39 +0530

    first commit

---
+++ authcart/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render,redirect
+
+# Create your views here.
+def signup(request):
+    return render(request,'/signup.html')
+def handlelogin(request):
+    return render(request,'/login.html')
+def handlelogout(request):
+    return redirect('/auth/login')
