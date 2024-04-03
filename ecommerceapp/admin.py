a52ec69f6f228559c028c7ffcd0c5010e631694d
niyaz-n <niyaznouzz@gmail.com>
2024-04-03 19:58:27 +0530

    payment gateway changes required

--- ecommerceapp/admin.py
+++ ecommerceapp/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from ecommerceapp.models import Contact,Product
+from ecommerceapp.models import Contact,Product,Orders,OrderUpdate
 # Register your models here.
+
 admin.site.register(Contact)
 admin.site.register(Product)
+admin.site.register(OrderUpdate)
+admin.site.register(Orders)
--- ecommerceapp/urls.py
+++ ecommerceapp/urls.py
@@ -6,8 +6,9 @@ urlpatterns = [
     path('contact',views.contact,name='contact'),
     path('about',views.about,name='about'),
     path('profile',views.profile,name='profile'),
-   # path('checkout',views.checkout,name='checkout'),
-    
+    path('checkout/',views.checkout,name='checkout'),
+    path('handlerequest/', views.handlerequest, name="HandleRequest"),
+
 
 
 ]
