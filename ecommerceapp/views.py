a078397211735aca8b9b73d99f89ab77952524a7
niyaz-n <niyaznouzz@gmail.com>
2024-04-04 12:28:59 +0530

    done

--- ecommerceapp/views.py
+++ ecommerceapp/views.py
@@ -7,12 +7,11 @@ from django.conf import settings
 MERCHANT_KEY=keys.MK
 import json
 from django.views.decorators.csrf import  csrf_exempt
-#from PayTm import Checksum
-import razorpay 
+from PayTm import Checksum
 
 # Create your views here.
 def index(request):
-    
+
     allProds = []
     catprods = Product.objects.values('category','id')
     print(catprods)
@@ -25,29 +24,25 @@ def index(request):
 
     params= {'allProds':allProds}
 
-
-    return render(request,'index.html',params)
-def contact(request):
-    if request.method=='POST':
-        name=request.POST.get('name')
-        email=request.POST.get('email')
-        desc=request.POST.get('desc')
-        pnumber=request.POST.get('pnumber')
-        myqwery=Contact(name=name,email=email,desc=desc,phonenumber=pnumber)
-        myqwery.save()
-        messages.info(request,'we will get back to you soon')
+    return render(request,"index.html",params)
 
     
-    return render(request,'contact.html')
+def contact(request):
+    if request.method=="POST":
+        name=request.POST.get("name")
+        email=request.POST.get("email")
+        desc=request.POST.get("desc")
+        pnumber=request.POST.get("pnumber")
+        myquery=Contact(name=name,email=email,desc=desc,phonenumber=pnumber)
+        myquery.save()
+        messages.info(request,"we will get back to you soon..")
+        return render(request,"contact.html")
 
 
+    return render(request,"contact.html")
 
 def about(request):
-    return render(request,'about.html')
-
-
-def profile(request):
-    return render(request,'profile.html')
+    return render(request,"about.html")
 
 
 
@@ -75,7 +70,7 @@ def checkout(request):
         thank = True
 # # PAYMENT INTEGRATION
 
- '''       id = Order.order_id
+        id = Order.order_id
         oid=str(id)+"ShopyCart"
         param_dict = {
 
@@ -126,4 +121,27 @@ def handlerequest(request):
             print("run agede function")
         else:
             print('order was not successful because' + response_dict['RESPMSG'])
-    return render(request, 'paymentstatus.html', {'response': response_dict})'''
+    return render(request, 'paymentstatus.html', {'response': response_dict})
+
+
+def profile(request):
+    if not request.user.is_authenticated:
+        messages.warning(request,"Login & Try Again")
+        return redirect('/auth/login')
+    currentuser=request.user.username
+    items=Orders.objects.filter(email=currentuser)
+    rid=""
+    for i in items:
+        print(i.oid)
+        # print(i.order_id)
+        myid=i.oid
+        rid=myid.replace("ShopyCart","")
+        print(rid)
+    status=OrderUpdate.objects.filter(order_id=int(rid))
+    for j in status:
+        print(j.update_desc)
+
+   
+    context ={"items":items,"status":status}
+    # print(currentuser)
+    return render(request,"profile.html",context)
